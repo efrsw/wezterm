c5a88fa4866194dc3fcaf8f28564731385073583
Egor Fursov <egorf@live.ru>
2025-08-11 10:34:20 +0300

    feat(linux): added linux specific config

--- config/font.lua
+++ config/font.lua
@@ -1,10 +1,15 @@
-local wezterm = require("wezterm")
+local wezterm = require "wezterm"
 
 local M = {}
 
 function M.apply(config)
-    config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
-    config.font_size = 12
+    if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+        config.font = wezterm.font "CaskaydiaCove Nerd Font Mono"
+        config.font_size = 14
+    else
+        config.font = wezterm.font "JetBrainsMono Nerd Font"
+        config.font_size = 22
+    end
 end
 
 return M
--- config/general.lua
+++ config/general.lua
@@ -1,7 +1,12 @@
+local wezterm = require("wezterm")
+
 local M = {}
 
 function M.apply(config)
-    config.default_prog = { "pwsh" }
+    if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+        config.default_prog = { "pwsh" }
+    end
+
     config.max_fps = 240
 end
 
